5859576d896aad18006e4ad442296a6c3af10a6b
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-06-19 13:02:07 +0530

    style manipulation

---
+++ Javascript/jQuery/index.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//    $("h1").css("color", "red");
+// });
+/* Not required as we have put our CDN link just above JS script tag*/
+
+$("h1").css("color", "green");
+$("h1").addClass("big-title");
---
+++ USEFUL LINKS/Minifiaction.md
@@ -0,0 +1,1 @@
+<a href="https://www.minifier.org/">Minify your JS/CSS code here!</a>
