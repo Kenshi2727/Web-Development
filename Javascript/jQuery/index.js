bd2819e6f5f028700771dfe4e5621e3baca72d89
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-06-19 15:35:59 +0530

    j Query completed

--- Javascript/jQuery/index.js
+++ Javascript/jQuery/index.js
@@ -3,5 +3,55 @@
 // });
 /* Not required as we have put our CDN link just above JS script tag*/
 
-$("h1").css("color", "green");
+$("h1").css("color", "white");
 $("h1").addClass("big-title");
+
+// $("button").text("Don't Click Me");
+$("button").html("<em>Hey Click Me<em>");
+/* The `<em>` tag in HTML is used to emphasize text, typically
+rendering it in italics. In this case, the text "Hey Click Me"
+inside the `<em>` tags will be displayed in italics on the
+button. */
+
+
+
+
+
+
+
+
+
+//attribute manipulation
+$("a").attr("href","https://www.pizzahut.co.in/");
+
+//adding event listeners using j query
+$("h1").click(function(){
+   $("h1").css("color","purple");
+})
+
+//adding event listeners using j query
+$("button").click(function(){
+   $("h1").css("color","blue");
+})
+
+
+$("input").keydown(function(event){
+   $("h1").text(event.key);
+});
+
+
+
+
+
+//adding animations using j query
+$("button").click(function(){
+   // $("h1").toggle();
+   // $("h1").fadeOut();
+   // $("h1").fadeIn();
+   // $("h1").fadeToggle();
+   // $("h1").slideToggle();
+
+
+   //chaining
+   $("h1").slideUp().slideDown().animate({opacity: 0.5});
+})
