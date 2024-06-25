9203d22d5ddf7e7c052df747a43404c7506ae9aa
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-06-25 23:49:50 +0530

    fixing nested repositories

--- Active Websites/Music Website UI/index.js
+++
@@ -1,8 +0,0 @@
-const menuOpen= $('#menu-open');
-const menuClose=$ ('#menu-close');
-const sidebar= $('.sidebar');
-
-menuOpen.click(()=>sidebar.css('left','0'));
-
-menuClose.click(()=>sidebar.css('left','-100%'));
-
