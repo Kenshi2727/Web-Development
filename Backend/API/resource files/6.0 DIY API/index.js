5156e94d8f0a2ad0d3814adbfc5fe7f2e2d43bd4
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-08-25 13:30:25 +0530

    completed custom api

--- Backend/API/resource files/6.0 DIY API/index.js
+++ Backend/API/resource files/6.0 DIY API/index.js
@@ -25,15 +25,71 @@ app.get('/filter',(req,res)=>{
   res.json(foundJokes);
 })
 //4. POST a new joke
-
+app.post('/jokes',(req,res)=>{
+  const text=req.body.text;
+  const type=req.body.type;
+  const  newId=jokes.length+1;
+  jokes.push({
+    id:newId,
+    jokeText:text,
+    jokeType:type
+  })
+  res.json({message:"Your joke is sent succesfully",
+            newJoke:jokes.slice(-1)
+          });
+});
 //5. PUT a joke
-
-//6. PATCH a joke
-
+app.put('/jokes/:id',(req,res)=>{
+  const id=parseInt(req.params.id);
+  //another method
+  //const searchIndex=findIndex((joke)=>joke.id===id);
+  jokes[id-1]={
+    id:id,
+    jokeText:req.body.text,
+    jokeType:req.body.type
+  }
+  res.json(jokes[id-1]);
+})
+//6. PATCH a joke(MUST CHECK SOLUTION)
+app.patch('/jokes/:id',(req,res)=>{
+  const id=parseInt(req.params.id);
+    if(req.body.text){
+       jokes[id-1].jokeText=req.body.text;
+    }
+    if(req.body.type){
+       jokes[id-1].jokeType=req.body.type;
+    }
+    // const replacementJoke = {
+    //   id: id,
+    //   jokeText: req.body.text || existingJoke.jokeText,
+    //   jokeType: req.body.type || existingJoke.jokeType,
+    // };
+  });
 //7. DELETE Specific joke
-
+app.delete('/jokes/:id',(req,res)=>{
+  const id=parseInt(req.params.id);
+  const searchIndex=jokes.findIndex((joke)=>joke.id===id);//returns -1 if no index found
+  if(searchIndex>-1){
+  jokes.splice(id-1,1);
+  res.json({message:"joke succesfully deleted!"});
+  }
+  else{
+    res.status(404)
+       .json({error:`No joke found for ${id}`});
+  }
+})
 //8. DELETE All jokes
-
+app.delete('/all',(req,res)=>{
+  const key=req.query.key;
+  if(key===masterKey){
+    jokes=[];
+    res.json({code:"Nuclear bomb detonated!"});
+  }
+  else{
+    res.status(404)
+       .json({error:"Inavlid Master Key!!!!!!!!"});
+  }
+})
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
 });
