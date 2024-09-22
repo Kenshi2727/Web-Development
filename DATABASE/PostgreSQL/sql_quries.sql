61a151462a508f6bca6d8e3fa037d12b2e752d46
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-09-22 10:37:03 +0530

    sql realtionships completed

--- DATABASE/PostgreSQL/sql_quries.sql
+++ DATABASE/PostgreSQL/sql_quries.sql
@@ -14,3 +14,12 @@ create table visited_countries(
    id serial primary key,
    country_code char(2)
 );
+
+--from many to many lesson no.-259--
+SELECT title
+FROM class
+JOIN enrollment AS e1 ON e1.class_id = class.id
+JOIN student AS s1 ON e1.student_id = s1.id
+JOIN enrollment AS e2 ON e2.class_id = class.id
+JOIN student AS s2 ON e2.student_id = s2.id
+WHERE s1.first_name = 'Angela' AND s2.first_name = 'Jack';
