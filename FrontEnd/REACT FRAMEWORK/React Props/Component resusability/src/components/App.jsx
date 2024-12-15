7536b39d2986ffb8a9617f5abe39cb4cef265719
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2024-12-15 13:28:38 +0530

    react dev tools

---
+++ FrontEnd/REACT FRAMEWORK/React Props/Component resusability/src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2023%2F08%2Fkenshi-yonezu-spinning-globe-the-boy-and-the-heron-theme-song-interview-000.jpg?w=960&cbr=1&q=90&fit=max" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
