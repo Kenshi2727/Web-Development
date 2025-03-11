1ba477b11fd964236d4b8cc088d3f0a97a63666b
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-03-11 20:58:31 +0530

    js-expressions and operators

---
+++ Programming Languages/Javascript/Practice Sets/expressions.js
@@ -0,0 +1,18 @@
+console.log("OPersators in js");
+let a = 45;
+let b = 78;
+console.log("The value of a**b is ", a ** b);
+
+//11
+//11
+//11
+//11
+//10
+//10
+
+let comp1 = 6;
+let comp2 = '6';
+console.log(comp1 == comp2);
+console.log(comp1 != comp2);
+console.log(comp1 === comp2);
+console.log(comp1 !== comp2);
