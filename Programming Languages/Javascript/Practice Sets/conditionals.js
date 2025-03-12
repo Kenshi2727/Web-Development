18cf058673381e60148b30f483bf0d0548bf9ee8
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-03-12 21:51:54 +0530

    js--conditionals

---
+++ Programming Languages/Javascript/Practice Sets/conditionals.js
@@ -0,0 +1,10 @@
+<script>
+    {prompt("Enter a number")}
+</script>
+let a = 'seven';
+console.log(typeof a);
+
+//coverting string to number
+// a = Number(a);
+a = Number.parseInt(a);
+console.log(typeof a);
---
+++ Programming Languages/Javascript/Practice Sets/set2.js
@@ -0,0 +1,38 @@
+let age = 19
+// if (age > 10 && age < 20)
+//     console.log("Teenager");
+// else
+//     console.log("Not a teenager");
+
+// switch (age) {
+//     case 15:
+//         console.log("You are a thief of 15 years old.");
+//         break;
+//     case 10:
+//         console.log("You are a thief of 10 years old.");
+//         break;
+//     case 11:
+//         console.log("You are sexy despite 11 years old.");
+//         break;
+//     default:
+//         console.log("You are a thief of unknown age.");
+// }
+
+
+// let num = 89;
+// if (num % 6 == 0)
+//     console.log("divisible by 2 and 3");
+// else
+//     console.log("not divisible by 2 and 3");
+// if (num % 2 == 0 && num % 3 == 0)
+//     console.log("divisible by 2 and 3");
+// else
+//     console.log("not divisible by 2 and 3");
+// if (num % 2 == 0 || num % 3 == 0)
+//     console.log("divisible by 2 or 3");
+// else
+//     console.log("not divisible by 2 or 3");
+
+age > 18 ? console.log("You can drive!!!") : console.log("You can't drive!!!");
+
+
