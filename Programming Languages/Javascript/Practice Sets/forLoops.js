02c97c9d07ec05e7ae5a6f0860869350aa7d71fe
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-03-13 13:35:56 +0530

    js loops

---
+++ Programming Languages/Javascript/Practice Sets/forLoops.js
@@ -0,0 +1,22 @@
+// for (let i = 0; i < 34; i++) {
+//     console.log(i);
+// }
+
+let obj = {
+    name: "Rohan Das",
+    age: 78,
+    type: "Dangerous Programmer",
+    os: "Ubuntu"
+}
+
+// for (let key in obj) {
+//     console.log(`The key of object is ${key}`);
+// }
+
+// for (let value of obj) {
+//     console.log(`The value of object is ${value}`);
+// }
+
+for (let s of "Ladka") {
+    console.log(s);
+}
